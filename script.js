15567074a7c0cbc8a20057337cae06a00c28328a
unknown <CodeKyng>
2026-07-15 10:25:58 +0100

    Changed the entire UI

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function add(a,b){
-    a + b;
-};
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target)
+        }
+    });
+}, {
+    threshold: 0.2
+});
+document.querySelectorAll('.reveal').forEach(img => {
+    observer.observe(img);
+})
